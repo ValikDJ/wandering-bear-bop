ef633023b050fd6d566ae5a64135b0f5f42e8282
[dyad] <valik.shevhyk@gmail.com>
2025-07-12 23:03:02 +0300

    [dyad] Виправлено синтаксичну помилку шляхом перейменування файлу utils.ts на utils.tsx та оновлення імпортів, а також покращено відображення результатів пошуку. - wrote 3 file(s), renamed 1 file(s)

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import React from "react";
+import React from "react"; // React is needed for JSX
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
