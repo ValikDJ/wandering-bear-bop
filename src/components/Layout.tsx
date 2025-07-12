ca7a51a4203067307d1e1f831baf54f1622644d0
[dyad] <valik.shevhyk@gmail.com>
2025-07-12 22:24:40 +0300

    [dyad] Виправлено проблему з фоном основної області контенту в темній темі. - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
       <Navbar />
-      <main className="flex-grow container mx-auto p-4">
+      <main className="flex-grow container mx-auto p-4 bg-background">
         {children}
       </main>
       <Footer />
